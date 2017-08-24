56caae385b930448dedd767cf64477b9db5ea1a1
lorenmanu <lorenrr1@gmail.com>
2017-08-24 10:29:41 +0200

    first commit

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,8 +10,7 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
